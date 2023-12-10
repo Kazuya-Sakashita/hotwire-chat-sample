724132a1ad4ff624cf30c18c39ad7919e36441b7
Kazuya-Sakashita <kz0508@gmail.com>
2023-12-10 18:07:20 +0900

    メッセージを評価

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,6 @@
 class Message < ApplicationRecord
   validates :content, presence: true
+  validates :evaluation, numericality: { only_integer: true, allow_blank: true }
 
   after_create_commit -> {
                         broadcast_append_to('messages', target: 'messages', partial: 'messages/message',
